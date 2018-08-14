c96917f683a041168d39dcad82f27ac77938538f
wangbin <wangbin@handlecar.com>
2018-08-14 16:36:06 +0800

    提交

--- src/main/java/com/gzzy/foodManProgram/configuration/BeanConfiguration.java
+++ src/main/java/com/gzzy/foodManProgram/configuration/BeanConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BeanConfiguration {
 
+
     @Bean
     public ObjectMapper objectMapper(){
         ObjectMapper objectMapper = new ObjectMapper();
